99f138f0567533930345ebf869535ace054a9c7b
Heewoong Noh <shgmldnd3@gmail.com>
2022-07-05 20:00:39 +0900

    Bayesian Personalized Ranking

--- BPR/train.py
+++ BPR/train.py
@@ -13,31 +13,6 @@ model_MF = BPR(gt_data,train_data,test_data,config['k'],config['learning_rate'],
 model_MF.train(config['epochs'],config['num_bootstrap'])
 
 
-'''
-factors = []
-tmp = []
-
-for K in range(40,46):
-    factors.append(K)
-    model_MF = BPR(gt_data,train_data,test_data,K,config['learning_rate'],config['cost_parameter'])
-    tmp.append(model_MF.train(config['epochs'],config['num_bootstrap']))
-    print(tmp)
-# plt.plot(factors,auc_list)
-# plt.show()
-print(tmp)
-
-'''
-
-
-
-
-
-
-
-
-
-
-
 ################################When using BPR based on model_kNN#################################
 
 
