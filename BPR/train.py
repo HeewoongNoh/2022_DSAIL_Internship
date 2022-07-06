127bd9ab20955ef31b58d578a0851b095b4f5624
Heewoong Noh <shgmldnd3@gmail.com>
2022-07-06 11:24:20 +0900

    Model_kNN

--- BPR/train.py
+++ BPR/train.py
@@ -1,20 +1,22 @@
 import yaml
 from model_MF import BPR_MF
+from model_kNN import BPR_kNN
 from data import ml_100k, train, test
 import matplotlib.pyplot as plt
 
 #Reading configuration yaml file for hyperparamters
 with open('configuration.yaml') as f:
     config = yaml.safe_load(f)
-
+'''
 ################################When using BPR based on model_MF##################################
 train_data, test_data, gt_data = train, test, ml_100k
 model_MF = BPR_MF(gt_data,train_data,test_data,config['k'],config['learning_rate'],config['cost_parameter'])
 model_MF.train(config['epochs'])
-
+'''
 
 ################################When using BPR based on model_kNN#################################
-
-
+train_data, test_data, gt_data = train, test, ml_100k
+model_kNN = BPR_kNN(gt_data,train_data,test_data,config['k'],config['learning_rate'],config['reg_pos'],config['reg_neg'])
+model_kNN.train(config['epochs'])
 
 ################################When using BPR based on model_MMMF################################
