74b708d11cfc6d9415d317e6c6600d764d27f2a4
Heewoong Noh <shgmldnd3@gmail.com>
2022-07-10 20:26:55 +0900

    Train(model_MF, model_kNN, model_WRMF)

--- BPR/train.py
+++ BPR/train.py
@@ -11,24 +11,24 @@ with open('configuration.yaml') as f:
     config = yaml.safe_load(f)
 
 # YOU CAN CHOOSE BPR_MR, BRP_kNN, WRMF WHICH RANKED IN TOP3, METRIC: AUC
-
-'''
-################################When using BPR based on model_MF##################################
+# Load data
 train_data, test_data, gt_data = train, test, ml_100k
+
+################################When using BPR based on model_MF##################################
+start = time.time()
 model_MF = BPR_MF(gt_data,train_data,test_data,config['k'],config['learning_rate'],config['cost_parameter'])
 model_MF.train(config['epochs'])
-
+end = time.time() - start
+print(f'total{end} time elapsed')
 
 ################################When using BPR based on model_kNN#################################
-train_data, test_data, gt_data = train, test, ml_100k
 start = time.time()
 model_kNN = BPR_kNN(gt_data,train_data,test_data,config['learning_rate'],config['reg_pos'],config['reg_neg'])
 model_kNN.train(config['epochs'])
 end = time.time() - start
 print(f'total{end} time elapsed')
-'''
+
 ################################When using BPR based on model_WRMF###############################
-train_data, test_data, gt_data = train, test, ml_100k
 start = time.time()
 model_WRMF = WRMF(gt_data,train_data,test_data,config['k'],config['alpha'],config['cost_parameter'])
 model_WRMF.train(config['epochs'])
